6a161f273cdb493013c075d77f4ef8ed72186c31
migue <mmoyano16@gmail.com>
2024-09-26 09:58:18 -0300

    test: disable routing middleware

--- src/middleware/auth.global.ts
+++ src/middleware/auth.global.ts
@@ -1,15 +1,15 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { loggedIn } = useUserSession();
-  const anonymousRoutes = ['/register', '/login'];
+  // const anonymousRoutes = ['/register', '/login'];
 
-  // Si el usuario no está logueado y no está yendo a /login, redirigir a /login
-  if (!loggedIn.value && !anonymousRoutes.includes(to.path)) {
-    return navigateTo('/login');
-  }
+  // // Si el usuario no está logueado y no está yendo a /login, redirigir a /login
+  // if (!loggedIn.value && !anonymousRoutes.includes(to.path)) {
+  //   return navigateTo('/login');
+  // }
 
-  // Si el usuario está logueado y trata de ir a /login, redirigir a la página de inicio
-  if (loggedIn.value && to.path === '/login') {
-    return navigateTo('/');
-  }
-
+  // // Si el usuario está logueado y trata de ir a /login, redirigir a la página de inicio
+  // if (loggedIn.value && to.path === '/login') {
+  //   return navigateTo('/');
+  // }
+  return;
 })
